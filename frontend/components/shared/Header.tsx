8ff450bfc763837f2db1da68d5e0a20b761c07e6
Alex <alexandre.pascal.ep@gmail.com>
2024-11-26 11:52:00 +0100

    Composant header

--- frontend/components/shared/Header.tsx
+++ frontend/components/shared/Header.tsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Search } from 'lucide-react';
 
 const Header = () => {
     return (
-        <header>
-
+        <header className="border-b border-gray-300">
+            <div className="container mx-auto">
+                <div className="flex justify-between items-center py-4">
+                    <div className="text-2xl">Logo</div>
+                    <div className="relative mt-4">
+                        {/* Icone de recherche */}
+                        <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+                        <input
+                            type="text"
+                            placeholder="Search..."
+                            className="w-full px-10 py-2 border rounded-md ml-1"
+                        />
+                    </div>
+                    <nav>
+                        <div className="flex items-center space-x-10">
+                            <ul className="flex space-x-4 text-lg">
+                                <li>Home</li>
+                                <li>About</li>
+                                <li>Contact</li>
+                            </ul>
+                            <input type='button' value='Login' className='bg-blue-500 text-white px-4 py-2 rounded-md' />
+                        </div>
+                    </nav>
+                </div>
+            </div>
         </header>
     )
 }
