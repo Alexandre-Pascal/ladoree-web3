86e26be6d534b77ea0c4113cf9ff5ba578eda8ef
Alex <alexandre.pascal.ep@gmail.com>
2024-11-26 18:35:16 +0100

    Add Artwork page and update ArtCard component for navigation; enhance global styles

--- frontend/components/shared/ArtCard.tsx
+++ frontend/components/shared/ArtCard.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 interface ArtCardProps {
     image: string;
@@ -10,30 +11,43 @@ interface ArtCardProps {
 
 const ArtCard: React.FC<ArtCardProps> = ({ image, title, artistImage, artist, price }) => {
     return (
-        <div
-            onClick={() => window.location.href = '/artwork'}
-            className="border p-4 rounded-md  transition-colors duration-300 hover:bg-gray-50 flex-none w-[325px] h-[450px] content-center justify-items-center cursor-pointer"
+        <Link
+            href={{
+                pathname: '/Artwork', // Page de destination
+                query: {
+                    image,
+                    title,
+                    artistImage,
+                    artist,
+                    price,
+                },
+            }}
+            className="border p-4 rounded-md transition-colors duration-300 hover:bg-gray-50 flex-none w-[325px] h-[450px] content-center justify-items-center cursor-pointer"
         >
-            <img
-                src={image}
-                alt="Oeuvre d'art"
-                className="w-[275px] h-[275px] object-cover rounded-md"
-            />
-            <div className="w-full mt-4 px-6">
-                <h3 className="text-xl text-gray-800 mt-4">{title}</h3>
+            <div>
+                <img
+                    src={image}
+                    alt="Oeuvre d'art"
+                    className="w-[275px] h-[275px] object-cover rounded-md"
+                />
+                <div className="w-full mt-4 px-6">
+                    <h3 className="text-xl text-gray-800 mt-4">{title}</h3>
 
-                <div className="flex items-center mt-2 gap-2">
-                    <img
-                        src={artistImage}
-                        alt="Artiste"
-                        className="w-6 h-6 object-cover rounded-full"
-                    />
-                    <p className="text-gray-600">{artist}</p>
+                    <div className="flex items-center mt-2 gap-2">
+                        <img
+                            src={artistImage}
+                            alt="Artiste"
+                            className="w-6 h-6 object-cover rounded-full"
+                        />
+                        <p className="text-gray-600">{artist}</p>
+                    </div>
+                    <p className="text-gray-800 mt-2 border border-white border-t-gray-200 pt-1 w">
+                        {price} €
+                    </p>
                 </div>
-                <p className="text-gray-800 mt-2 border border-white border-t-gray-200 pt-1 w">{price} €</p>
             </div>
-        </div>
-    )
-}
+        </Link>
+    );
+};
 
-export default ArtCard
+export default ArtCard;
