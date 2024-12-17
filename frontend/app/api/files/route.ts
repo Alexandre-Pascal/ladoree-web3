2046842c35e2d3f0cb9f21e41683c919c2a199c1
Alex <alexandre.pascal.ep@gmail.com>
2024-12-17 18:53:33 +0100

    Refactor file upload handling to use environment variable for gateway URL

--- frontend/app/api/files/route.ts
+++ frontend/app/api/files/route.ts
@@ -6,7 +6,11 @@ export async function POST(request: NextRequest) {
         const data = await request.formData();
         const file: File | null = data.get("file") as unknown as File;
         const uploadData = await pinata.upload.file(file)
-        const url = await pinata.gateways.convert(uploadData.IpfsHash)
+        console.log(file, "file");
+        console.log(uploadData, "uploadData");
+        // const url = await pinata.gateways.convert(uploadData.IpfsHash)
+        const url = `https://${process.env.NEXT_PUBLIC_GATEWAY_URL}/${uploadData.IpfsHash}`;
+        console.log(url, "url");
         return NextResponse.json(url, { status: 200 });
     } catch (e) {
         console.log(e);
